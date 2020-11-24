535a9561386ed6984e5855941a25405d10feb18d
pham van thinh <phamvanthinh2001x@gmail.com>
2020-11-24 15:04:30 +0700

    cookies

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -15,6 +15,7 @@ module.exports.search = (req,res)=>{
     });
 };
 module.exports.create =  (req,res)=>{
+    console.log(req.cookies);
     res.render('users/create');
 };
 module.exports.get = (req,res)=>{
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const { render } = require('pug');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');// npm install cookie-parser de doc duoc cookie cho create
 
 var userRoute = require('./routes/user.route');
 
@@ -13,6 +14,7 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use(cookieParser());
 
 app.get('/',(req,res)=>{
     res.render('index',{
