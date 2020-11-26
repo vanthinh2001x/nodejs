292ac19947b06028dbe58e04adbc120cd858c8e2
pham van thinh <phamvanthinh2001x@gmail.com>
2020-11-26 08:35:36 +0700

    debug in tooldev

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+console.log(process.env.SESSION_SECRET);
 var express = require('express');
 const { render } = require('pug');
 var bodyParser = require('body-parser');
@@ -17,7 +19,7 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-app.use(cookieParser('asbkakdsjfljks10230'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.get('/',(req,res)=>{
     res.render('index',{
