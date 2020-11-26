123ace67dccae1e501972f98903356b627db965e
pham van thinh <phamvanthinh2001x@gmail.com>
2020-11-26 10:50:15 +0700

    add avatar

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -27,8 +27,7 @@ module.exports.get = (req,res)=>{
 };
 module.exports.postCreate =(req,res)=>{
     req.body.id = shortid.generate();
-
-    console.log(res.locals);
+    req.body.avatar = req.file.path.split("").slice(7).join('');
 
     db.get('users').push(req.body).write();
     res.redirect('/users')
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,8 @@ var express = require('express');
 var controller = require('../controllers/user.controllers');
 var validate = require('../validate/user.validate');
 var authMiddleware = require('../middlewares/auth.middlewares');
-
+var multer = require('multer');
+var upload = multer({dest: './public/uploads/'});
 var router = express.Router();
 
 router.get('/', controller.index);
@@ -11,7 +12,14 @@ router.get('/cookie',function(req,res,next){
     res.send('Hello');
 })
 router.get('/search',controller.search);
+
 router.get('/create',controller.create);
+
 router.get('/:id',controller.get);
-router.post('/create',validate.postCreate, controller.postCreate);
+
+router.post('/create',
+ upload.single('avatar'),
+ validate.postCreate,
+ controller.postCreate);
+
 module.exports = router;
